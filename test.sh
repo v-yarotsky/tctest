68f1e771318cd513fd2c3aad3283f83182e8a216
Vladimir Yarotsky <v.yarotsky@itransition.com>
2014-03-10 11:16:41 +0300

    failure

--- test.sh
+++ test.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
 echo $1
-RESULT=${1:-success.xml}
-touch $RESULT
-cat $RESULT
+RESULT=${1:-success}
+touch "$RESULT.xml"
+cp "$RESULT.sample.xml" "$RESULT.xml"
+
+case $RESULT in
+  success)
+    exit 0
+    ;;
+  failure)
+    exit 1
+    ;;
+esac
 
